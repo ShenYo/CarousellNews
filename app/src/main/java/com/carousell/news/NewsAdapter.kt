316015df7180a095ecc51d5a727d72bdadd1b03b
lancewang <shenyo.wang@gmail.com>
2019-03-30 23:23:35 +0800

    [News] bind mock data to news item view

--- app/src/main/java/com/carousell/news/NewsAdapter.kt
+++ app/src/main/java/com/carousell/news/NewsAdapter.kt
@@ -8,9 +8,13 @@ import com.carousell.base.BaseRecyclerViewAdapter
 import com.carousell.base.BaseRecyclerViewHolder
 import com.carousell.imageloader.GlideApp
 import kotlinx.android.synthetic.main.item_news.view.*
+import org.threeten.bp.Instant
+import org.threeten.bp.LocalDate
+import org.threeten.bp.Period
+import org.threeten.bp.ZoneId
 
 class NewsAdapter(
-     context: Context
+    context: Context
 ) : BaseRecyclerViewAdapter<NewsModel>(context) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsItemViewHolder {
@@ -26,11 +30,48 @@ class NewsAdapter(
 class NewsItemViewHolder(itemView: View) : BaseRecyclerViewHolder<NewsModel>(itemView) {
 
     override fun bindDataToView(data: NewsModel) {
+        bindToBanner(data.bannerUrl)
+        bindToTitle(data.title)
+        bindToDescription(data.description)
+        bindToCreateTime(data.timeCreated)
+    }
+
+    private fun bindToBanner(bannerUrl: String) {
         GlideApp.with(itemView.context)
-            .load("https://storage.googleapis.com/carousell-interview-assets/android/images/carousell-siu-rui-ceo-tia-sg-2018.jpg")
+            .load(bannerUrl)
             .error(R.drawable.ic_launcher_background)
             .into(itemView.banner)
     }
 
+    private fun bindToTitle(title: String) {
+        itemView.textTitle.text = title
+    }
+
+    private fun bindToDescription(description: String) {
+        itemView.textDescription.text = description
+    }
+
+    private fun bindToCreateTime(timeCreated: Long) {
+        val period = Period.between(
+            Instant.ofEpochSecond(timeCreated).atZone(ZoneId.systemDefault()).toLocalDate(),
+            LocalDate.now()
+        )
+
+        itemView.textCreatedAt.text = when {
+            period.years > 0 -> {
+                "${period.years} years ago"
+            }
+            period.months > 0 -> {
+                "${period.months} months ago"
+            }
+            period.days / 7 >= 4 -> {
+                "${period.days / 7} weeks ago"
+            }
+            else -> {
+                "${period.days} weeks ago"
+            }
+        }
+
+    }
 
 }
--- app/src/main/java/com/carousell/news/NewsModel.kt
+++ app/src/main/java/com/carousell/news/NewsModel.kt
@@ -1,5 +1,18 @@
 package com.carousell.news
 
-class NewsModel {
+import com.google.gson.annotations.SerializedName
 
-}
+data class NewsModel(
+    @SerializedName("description")
+    val description: String,
+    @SerializedName("time_created")
+    val timeCreated: Long,
+    @SerializedName("rank")
+    val rank: Int,
+    @SerializedName("banner_url")
+    val bannerUrl: String,
+    @SerializedName("id")
+    val id: String,
+    @SerializedName("title")
+    val title: String
+)
